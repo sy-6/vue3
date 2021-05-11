a894fbfeff15f11b4192a5f1d284373b8cd2bcf0
sy-6 <249859693qq.com>
2021-05-11 20:18:19 +0800

    bbb

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <Box />
-    
+    <B>
   </div>
 </template>
 
 <script>
+import B from './components/B.vue';
 import Box from "./components/Box.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
@@ -15,7 +16,8 @@ export default {
   name: "App",
   components: {
     HelloWorld,
-    Box
+    Box,
+    B
   },
 };
 </script>
